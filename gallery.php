7235fde0edc1b57f8684c91b5d7cd5c6a0a168ae
charuka <Chrk4.pro@gmail.com>
2023-10-28 18:21:48 +0530

    add main user fuction to resturent mangemnt system

---
+++ gallery.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+div.gallery {
+  border: 1px solid #ccc;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.responsive {
+  padding: 0 6px;
+  float: left;
+  width: 24.99999%;
+}
+
+@media only screen and (max-width: 700px) {
+  .responsive {
+    width: 49.99999%;
+    margin: 6px 0;
+  }
+}
+
+@media only screen and (max-width: 500px) {
+  .responsive {
+    width: 100%;
+  }
+}
+
+.clearfix:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
+
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">   
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>Home</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+
+    <header id="header" class="header-scroll top-header headrom">
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="" alt="">Bilal Restaurant </a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Dishes <span class="sr-only"></span></a> </li>
+                            
+                            <li class="nav-item"> <a class="nav-link active" href="reseversion/index.php">Reserve Table <span class="sr-only"></span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="reseversion/index.php">Facilities <span class="sr-only"></span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="gallery.php">Gallery <span class="sr-only"></span></a> </li>
+                           
+                            <?php
+                        if(empty($_SESSION["user_id"])) // if user is not login
+                            {
+                                echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
+                              <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
+                            }
+                        else
+                            {
+
+                                    
+                                    echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
+                                    echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
+                            }
+
+                        ?>
+                             
+                        </ul>
+                         
+                    </div>
+                </div>
+            </nav>
+
+        </header>
+<br><br><br><br>
+<h2 style="text-align:center;">Image Gallery Coming Soon</h2>
+
+</body>
+</html>
+
+
